412533a9bf3375f0028e0c2a8bfcdfc465856b75
jorgegvalencia <jorgegvalencia@gmail.com>
2016-03-15 23:14:57 +0100

    Script de inicializacion de la BD con anuncios.

---
+++ init_db.js
@@ -0,0 +1,42 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var fs = require('fs');
+var async = require('async');
+var conn = require('./lib/mongooseManager');
+
+require('./model/anuncio_model');
+
+var Anuncio = mongoose.model('anuncios');
+
+function setDatabaseAds() {
+	mongoose.connection.collections['anuncios'].drop( function(err) {
+    	console.log('collection dropped');
+    	if (err) {
+        	console.log(err);
+            return;
+        }
+         fs.readFile('./anuncios.json', 'utf8', function(err, data) {
+            if (err) {
+            	return;
+            } else {
+                var anuncios = JSON.parse(data);
+                async.eachSeries(anuncios.anuncios,
+                    function(item, next) {
+                    	var anuncio = new Anuncio(item);
+                    	console.log(anuncio);
+                        anuncio.save(function(err){
+                        	next();
+                        });
+                    },
+                    function end(err) {
+                        mongoose.connection.close();
+                    }
+                );
+            }
+        });
+	});
+}
+
+setDatabaseAds();
+
